175e96f968f5b3007fec9a67882f317a6ad9d175
gorillazrule <t7hernandez@gmail.com>
2025-12-05 13:26:32 -0500

    Day1 Part1

---
+++ Day1/Part1/d1_p1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <string>
+
+
+int main()
+{
+	//Read in all the operations from the input
+	std::ifstream infile("../input.txt");
+	
+	std::string temp;
+	std::queue<int> operations;
+	while(std::getline(infile, temp))
+	{
+		int distance = std::stoi(temp.substr(1));
+		//If counterclockwise rotation, multiply by -1.
+		if(temp.at(0) == 'L')
+		{
+			distance *= -1;
+		}
+		operations.push(distance);
+	}
+	infile.close();
+	
+	//Start performing the operations on the safe, keeping track of every time the dial points to zero.
+	int pos = 50;
+	int count = 0;
+	while(!operations.empty())
+	{
+		pos += operations.front();
+		operations.pop();
+		while(pos > 99)
+		{
+			pos -= 100;
+		}
+		while(pos < 0)
+		{
+			pos += 100;
+		}
+		
+		if(pos == 0)
+		{
+			++count;
+		}
+	}
+	
+	std::cout << count << std::endl;
+	
+	return 0;	
+}
